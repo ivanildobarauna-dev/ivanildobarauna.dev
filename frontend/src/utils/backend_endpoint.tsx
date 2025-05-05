697a5bf6263cf0a975742cd9f9f7978771f8252f
Ivanildo Barauna de Souza Junior <ivanildo.jnr@outlook.com>
2025-05-04 22:30:58 -0300

    chore: backend endpoint

--- frontend/src/utils/backend_endpoint.tsx
+++ frontend/src/utils/backend_endpoint.tsx
@@ -5,9 +5,10 @@ export function getBackendEndpoint(endpoint: string) {
   let baseUrl;
   
   if (isClient) {
-    // No cliente (browser), sempre use o host da atual página
+    // No cliente (browser), sempre use o host da atual página e o protocolo atual
     const hostname = window.location.hostname;
-    baseUrl = `http://${hostname}:8090/api/v1`;
+    const protocol = window.location.protocol;
+    baseUrl = `${protocol}//${hostname}:8090/api/v1`;
   } else {
     // No servidor (SSR), use o nome do serviço Docker em produção
     baseUrl = process.env.NODE_ENV === 'production' 
