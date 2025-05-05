3fb64a0435e1c334a5c06ccc407f053eae7fcfe9
Ivanildo Barauna de Souza Junior <ivanildo.jnr@outlook.com>
2025-05-05 09:39:19 -0300

    refactor: SQLIte adapter to reuse the StaticPool

--- backend/src/infrastructure/adapters/outbound_sql_lite_adapter.py
+++ backend/src/infrastructure/adapters/outbound_sql_lite_adapter.py
@@ -1,7 +1,9 @@
 from typing import Type
+from contextlib import contextmanager
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.pool import StaticPool
 
 from src.domain.dto.certification import Certification
 from src.domain.dto.experience import Experience
@@ -12,25 +14,38 @@ from src.infrastructure.ports.repository_interface import RepositoryInterface
 
 
 class SqlLiteAdapter(RepositoryInterface):
-    """
-    Adapter for outbound SQL Lite database operations.
-    """
-
     def __init__(self, database_path: str) -> None:
-        self.engine = create_engine(database_path, echo=False)
-        self.session = sessionmaker(bind=self.engine)
+        self.engine = create_engine(
+            database_path,
+            echo=False,
+            connect_args={"check_same_thread": False},
+            poolclass=StaticPool,
+        )
+        self.session_factory = scoped_session(sessionmaker(bind=self.engine))
 
-    def get_all_projects(self) -> list[Type[Project]]:
-        return self.session().query(Project).all()
+    @contextmanager
+    def get_session(self):
+        session = self.session_factory()
+        try:
+            yield session
+        finally:
+            session.close()
 
-    def get_all_certifications(self) -> list[Type[Certification]]:
-        return self.session().query(Certification).all()
+    def get_all(self, model_class: Type) -> list:
+        with self.get_session() as session:
+            return session.query(model_class).all()
 
-    def get_all_formations(self) -> list[Type[Formation]]:
-        return self.session().query(Formation).all()
+    def get_all_projects(self) -> list[Project]:
+        return self.get_all(Project)
 
-    def get_all_experiences(self) -> list[Type[Experience]]:
-        return self.session().query(Experience).all()
+    def get_all_certifications(self) -> list[Certification]:
+        return self.get_all(Certification)
 
-    def get_all_social_media(self) -> list[Type[SocialMedia]]:
-        return self.session().query(SocialMedia).all()
+    def get_all_formations(self) -> list[Formation]:
+        return self.get_all(Formation)
+
+    def get_all_experiences(self) -> list[Experience]:
+        return self.get_all(Experience)
+
+    def get_all_social_media(self) -> list[SocialMedia]:
+        return self.get_all(SocialMedia)
